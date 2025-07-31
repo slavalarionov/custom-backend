eaf12d0ce71db65dc4dba08de32dfd4ecd839102
Daniil Jasper <jasper@deeplay.io>
2025-07-31 12:02:00 +0600

    fix

--- retailcrm.php
+++ retailcrm.php
@@ -1,6 +1,6 @@
 <?php
-require_once __DIR__ . '/../vendor/autoload.php';
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+require_once __DIR__ . '/vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 // api/retailcrm.php
 
